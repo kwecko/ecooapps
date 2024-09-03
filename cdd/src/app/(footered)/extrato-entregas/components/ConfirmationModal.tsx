0fef51f7ee7f1bc2016d9810bbbc74df413d8c7f
LucasMachadoG <lucasgaelzermachado@gmail.com>
2024-09-03 18:11:47 -0300

    refactor: conflict resolve

--- cdd/src/app/(footered)/extrato-entregas/components/ConfirmationModal.tsx
+++ cdd/src/app/(footered)/extrato-entregas/components/ConfirmationModal.tsx
@@ -1,24 +1,24 @@
-import React, { ReactNode } from "react";
-import Modal from "@cdd/components/Modalll";
+// import React, { ReactNode } from "react";
+// import Modal from "@cdd/components/Modal";
 
-interface ConfirmationModalProps {
-  openButton: ReactNode;
-  link: string;
-}
+// interface ConfirmationModalProps {
+//   openButton: ReactNode;
+//   link: string;
+// }
 
-export default function ConfirmationModal({
-  openButton,
-  link,
-}: ConfirmationModalProps) {
-  return (
-    <Modal
-      openButton={openButton}
-      title="Imprimir extrato"
-      description="Faça a impressao do extrato para a entrega dos motoboy"
-      approvalButtons={true}
-      textButton1="Cancelar"
-      textButton2="Imprimir"
-      bgButton2="#00735E"
-    />
-  );
-}
+// export default function ConfirmationModal({
+//   openButton,
+//   link,
+// }: ConfirmationModalProps) {
+//   return (
+//     <Modal
+//       ={openButton}
+//       title="Imprimir extrato"
+//       description="Faça a impressao do extrato para a entrega dos motoboy"
+//       approvalButtons={true}
+//       textButton1="Cancelar"
+//       textButton2="Imprimir"
+//       bgButton2="#00735E"
+//     />
+//   );
+// }
