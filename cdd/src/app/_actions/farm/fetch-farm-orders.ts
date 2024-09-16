efb7fb7bc8291e2f07a2274fb506883d8186acac
lucaslimasz <id.lucas@outlook.com.br>
2024-09-16 19:29:46 -0300

    feat: actions

--- cdd/src/app/_actions/farm/fetch-farm-orders.ts
+++ cdd/src/app/_actions/farm/fetch-farm-orders.ts
@@ -2,20 +2,14 @@
 
 import ApiService from "@shared/service/index";
 
-import data from "@cdd/app/(protected)/(footered)/ofertas/components/farm-datail.json";
-
 interface FetchFarmOrdersRequest {
-  farm_id: string;
-  cycle_id: string;
+  box_id: string;
 }
 
-export async function fetchFarmOrders({
-  farm_id,
-  cycle_id,
-}: FetchFarmOrdersRequest) {
-  // const response = ApiService.GET({
-  //   url: `/orders/${farm_id}?cycle_id=${cycle_id}`
-  // })
+export async function fetchFarmOrders({ box_id }: FetchFarmOrdersRequest) {
+  const response = ApiService.GET({
+    url: `/boxes/${box_id}`,
+  });
 
-  return { data: data };
+  return response;
 }
