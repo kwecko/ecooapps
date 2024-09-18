dff1580e07f37e519554362a00002a025d1bc8ce
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-09-17 23:10:40 -0300

    fix: apply changes to the offer card related to the layout of the component and price displaying

--- consumer/src/app/(purchase)/ofertas/components/card-oferta.tsx
+++ consumer/src/app/(purchase)/ofertas/components/card-oferta.tsx
@@ -105,63 +105,119 @@ export default function CardOferta({
   };
 
   return (
-    <div className="min-w-[350px] min-h-[156px] bg-[rgb(246,246,246)] flex rounded-2xl ml-2.5 mr-6 mt-4 mb-4">
-      <div className="flex-none relative h-24 w-24 rounded-xl mt-3 mr-3 ml-3 mb-11">
-        <Image
-          className="rounded-xl"
-          loader={imageLoader}
-          src={offer.product.image}
-          width={100}
-          height={100}
-          alt={`${offer.product.name.toLowerCase()}.jpg`}
-        />
-        <div className="absolute flex justify-center bottom-0 right-0 bg-[#3E5155] text-white items-center w-12 h-5 rounded-tl-xl rounded-br-xl text-xs">
-          {mapTextQuantity[offer.product.pricing]}
+    <div className="w-[350px] bg-[rgb(246,246,246)] flex flex-col rounded-2xl justify-start p-2.5 items-start">
+      {/* Product div */}
+      <div className="flex flex-row w-full gap-2.5">
+        <div className="relative h-24 w-24 rounded-xl shrink-0">
+          <Image
+            className="rounded-xl h-24 w-24"
+            loader={imageLoader}
+            src={offer.product.image}
+            width={100}
+            height={100}
+            alt={`${offer.product.name.toLowerCase()}.jpg`}
+          />
+          <div className="absolute flex justify-center bottom-0 right-0 bg-[#3E5155] text-white items-center w-12 h-5 rounded-tl-xl rounded-br-xl text-xs">
+            {mapTextQuantity[offer.product.pricing]}
+          </div>
+        </div>
+        <div className="flex flex-col w-full h-full justify-between">
+          <div className="flex flex-col justify-between w-full">
+            <p className="w-full text-left font-poppins text-base leading-5 font-bold
+            text-[#2F4A4D]">
+              {offer.product.name}
+            </p>
+            {/* Description div */}
+            <div className="w-[calc(100%-2.5rem)]">
+              {offer.description && offer.description.length <= 70 ? (
+                <p className="w-full text-left font-poppins text-xs text-[#2F4A4D]">
+                  {offer.description}
+                </p>
+              ) : null}
+
+              {offer.description && offer.description.length > 70 ? (
+                <>
+                  <p className="w-full text-left font-poppins text-xs text-[#2F4A4D]">
+                    {offer.expandDescription
+                      ? offer.description
+                      : `${offer?.description?.slice(0, 70)}...`}
+                  </p>
+                  <p>
+                    <button
+                      onClick={() => toggleDescription(offer)}
+                      className="text-[#2F4A4D] text-xs font-semibold underline"
+                    >
+                      {offer.expandDescription ? "Fechar" : "Leia mais"}
+                    </button>
+                  </p>
+                </>
+              ) : null}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col w-52 min-h-28 m-2.5">
-        <div className="grow">
-          <p className="w-full text-left font-poppins text-base text-[#2F4A4D]">
-            {offer.product.name}
+
+      {/* Order div */}
+      <div className="w-full flex flex-row justify-between gap-2.5">
+        {/* Price div */}
+        <div className="flex flex-col items-end w-full">
+          <p className="font-poppins text-xs font-bold text-battleship-gray">
+            {offer.product.pricing === "UNIT" && (
+              <span>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(offer.price)}<span className="text-[10px]">/unid.</span>
+              </span>
+            )}
+            {offer.product.pricing === "WEIGHT" && (
+              <span>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(offer.price / 2)}<span className="text-[10px]">/500g</span>
+              </span>
+            )}
           </p>
-          {offer.description && offer.description.length <= 70 ? (
-            <div>
-              <p className="w-full text-left font-poppins text-xs text-[#2F4A4D]">
-                {offer.description}
-              </p>
-            </div>
-          ) : null}
-
-          {offer.description && offer.description.length > 70 ? (
-            <div>
-              <p className="w-full text-left font-poppins text-xs text-[#2F4A4D]">
-                {offer.expandDescription
-                  ? offer.description
-                  : `${offer?.description?.slice(0, 70)}...`}
-              </p>
-              <p>
-                <button
-                  onClick={() => toggleDescription(offer)}
-                  className="text-[#2F4A4D] text-xs font-semibold underline"
-                >
-                  {offer.expandDescription ? "Leia menos" : "Leia mais"}
-                </button>
-              </p>
-            </div>
-          ) : null}
+          <p className="font-poppins text-lg font-bold text-slate-gray">
+            {count > 0 ? (
+              <>
+                {
+                  offer.product.pricing === "UNIT" && (
+                    <span>
+                      {(
+                        (offer.price) * count
+                      ).toLocaleString("pt-br", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </span>
+                  )
+                }
+                {offer.product.pricing === "WEIGHT" && (
+                  <span>
+                    {(
+                      (offer.price / 2) * count
+                    ).toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}<span className="text-sm">/{count * 0.5}kg</span>
+                  </span>
+                )}
+              </>
+            ) : (
+              <span>
+                {(0).toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            )}
+          </p>
         </div>
-        <div className="flex flex-row min-h-8">
-          <div className="w-[124px] mr-2.5">
-            <p className="w-full text-left font-poppins text-base text-[#2F4A4D] pt-3">
-              {offer.price.toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-              })}
-              /{mapPriceText[offer.product.pricing]}
-            </p>
-          </div>
-
-          <div className="flex-none bg-white rounded-md flex flex-row w-[86px] h-[30px] mt-2.5">
+        {/* Control amount div */}
+        <div className="flex flex-col justify-end items-end w-[86px]">
+          <div className="bg-white rounded-md flex flex-row w-[86px] h-7">
             <div className="flex items-center justify-center">
               <button
                 type="button"
@@ -186,7 +242,7 @@ export default function CardOferta({
                 type="button"
                 className={
                   count !=
-                  Math.floor(offer.amount / mapQuantity[offer.product.pricing])
+                    Math.floor(offer.amount / mapQuantity[offer.product.pricing])
                     ? "text-[#00735E] text-2xl p-1.5"
                     : "text-[#00735E] text-2xl p-1.5 opacity-25"
                 }
@@ -202,6 +258,6 @@ export default function CardOferta({
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
