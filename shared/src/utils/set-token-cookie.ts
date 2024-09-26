5a5fa003b447f9dcf15efede82dbc9aa4b3d31cc
LucasMachadoG <lucasgaelzermachado@gmail.com>
2024-09-26 18:05:51 -0300

    fix: environment variables

--- shared/src/utils/set-token-cookie.ts
+++ shared/src/utils/set-token-cookie.ts
@@ -1,10 +1,12 @@
 import { cookies } from "next/headers"
 
 export const SetTokenCookie = (token: string) => {
-  cookies().delete("token")
-  cookies().set("token", token, {
-    httpOnly: process.env.ENV === "production" || process.env.ENV === "homolog",
-    secure: process.env.ENV === "production" || process.env.ENV === "homolog",
+  const Cookies = cookies()
+
+  Cookies.delete("token")
+  Cookies.set("token", token, {
+    httpOnly: process.env.NODE_ENV !== 'development',
+    secure: process.env.NODE_ENV !== 'development',
     sameSite: "lax",
     path: "/",
   })
