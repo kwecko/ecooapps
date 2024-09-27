2a5233e8dbee88b9f90af10446222698e27d92df
Gabriel T. Jaeger <23436587+gabrieltjaeger@users.noreply.github.com>
2024-09-27 17:59:44 -0300

    Update page.tsx

--- producer/src/app/teste/page.tsx
+++ producer/src/app/teste/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <p>
-        Esta página serve para demonstrar que o middleware está funcionando
+        Esta página serve para mostrar que o middleware está funcionando
         juntamente com a página em construção.
       </p>
     </div>
