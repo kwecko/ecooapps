0831d886866b0cbae795c5f95a28c361821daca3
LucasMachadoG <lucasgaelzermachado@gmail.com>
2024-10-02 20:36:37 -0300

    refactor: useHandleError

--- shared/src/errors.ts
+++ shared/src/errors.ts
@@ -1,14 +1,27 @@
   export const errorsMapper: { [key: string]: string } = {
     "Sessão expirada.": "Sua sessão expirou. Por favor, faça login novamente.",
     "💥 Ocorreu um erro interno.": "Ocorreu um erro interno. Tente novamente mais tarde.",
-    "As credenciais de acesso não são válidas.": "Credenciais inválidas.",
+    "As credenciais de acesso não são válidas.": "Suas credênciais estão inválidas.",
     "Erro desconhecido": "Erro desconhecido.",
     "Erro de validação.": "Erro de validação.",
     "Você está tentando acessar um app apenas para administradores!": "Você está tentando acessar um app apenas para administradores!",
     "Você está tentando acessar um app apenas para administradores ou produtores!": "Você está tentando acessar um app apenas para administradores ou produtores!",
     "Você está tentando acessar um app apenas para administradores ou consumidores!": "Você está tentando acessar um app apenas para administradores ou consumidores!",
-    "O usuário não está verificado.": "Seu usuário não está autenticado.",
+    "O usuário não está verificado.": "Sua conta não está autenticada. Por favor, verifique o seu e-mail.",
     "Não autorizado": "Acesso não autorizado.",
     "O agronegócio não está ativo.": "O seu agronegócio não está ativo! Por favor entre em contato com o CDD.",
     "Essa conta não tem uma senha definida.": "Sua conta não possui uma senha definida.",
   };
+
+  export const genericErrorsMapper = [
+    "Ciclo",
+    "Fazenda",
+    "Oferta",
+    "Sacola",
+    "Usuário",
+    "ofertado",
+    "solicitado",
+    "ofertar",
+    "enviar",
+    "comprar"
+  ]
