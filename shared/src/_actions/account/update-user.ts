879e9f135fa094c5be3887110dacff02e10cf5d5
lucaslimasz <id.lucas@outlook.com.br>
2024-10-09 18:52:51 -0300

    feat: implementation reset password

--- shared/src/_actions/account/update-user.ts
+++ shared/src/_actions/account/update-user.ts
@@ -1,13 +1,13 @@
-"use server"
+"use server";
 
-import ApiService from "../../service/index"
-import { IUserUpdate } from "../../interfaces/update.user";
+import ApiService from "../../service/index";
+import { User as IUser } from "../../interfaces/user";
 
-export async function updateUser(data: IUserUpdate) {
-    const response = ApiService.PATCH({
-    url: '/users',
-    data
-  })
+export async function updateUser(data: IUser) {
+  const response = ApiService.PATCH({
+    url: "/users",
+    data,
+  });
 
   return response;
 }
