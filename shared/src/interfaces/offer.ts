ec7e4a2bf4b72da2ae3d47956d3bff59ff921eac
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-10-15 10:36:01 -0300

    refactor: use OfferWithProductDTO in the IPendingDeliveries and remove duplicate interfaces

--- shared/src/interfaces/offer.ts
+++ shared/src/interfaces/offer.ts
@@ -1,29 +1,12 @@
+import { OfferWithProductDTO } from '@shared/domain/dtos/offer-with-product-dto';
+
+
 export interface IPendingDeliveries {
   id: string;
   bag_id: string;
-  offer: Offer;
+  offer: OfferWithProductDTO;
   status: string;
   amount: number;
-  created_at: string;
-  updated_at: any;
-}
-
-export interface Offer {
-  id: string;
-  amount: number;
-  price: number;
-  description: any;
-  catalog_id: string;
-  product: Product;
-  created_at: string;
-  updated_at: any;
-}
-
-export interface Product {
-  id: string;
-  name: string;
-  image: string;
-  pricing: string;
   created_at: string;
   updated_at: any;
 }
