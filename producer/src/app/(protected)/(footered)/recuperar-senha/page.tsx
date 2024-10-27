67fbe4d4061b0ab80c74a9e5f3f9b06181c803fa
lucaslimasz <id.lucas@outlook.com.br>
2024-10-26 23:45:22 -0300

    refactor: schema, CustomInput, spacing, config tailwind

--- producer/src/app/(protected)/(footered)/recuperar-senha/page.tsx
+++ producer/src/app/(protected)/(footered)/recuperar-senha/page.tsx
@@ -1,20 +1,31 @@
-'use client'
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from "sonner";
+
+import { useForm } from "react-hook-form";
 
 import { resetPassword } from '@producer/app/_actions/user/reset-password';
 import Loading from '@producer/app/loading';
-import ButtonV2 from '@shared/components/ButtonV2';
-import Input from '@shared/components/Input';
-import { ModelPage } from '@shared/components/ModelPage';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { schemaForgotPassword } from "@shared/schemas/forgot-password";
 
-import { useForm } from 'react-hook-form';
-import { toast } from 'sonner';
+import { zodResolver } from '@hookform/resolvers/zod';
+import ButtonV2 from '@shared/components/ButtonV2';
+import CustomInput from '@shared/components/CustomInput';
+import { ModelPage } from '@shared/components/ModelPage';
 
 function RecuperarSenha() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { register, handleSubmit } = useForm<{ email: string }>();
   const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ email: string }>({
+    resolver: zodResolver(schemaForgotPassword),
+  });
 
   const onSubmit = async (email: { email: string }) => {
     try {
@@ -22,18 +33,19 @@ function RecuperarSenha() {
       const data = await resetPassword(email);
 
       if (data.message) {
-        return toast.error(data.message)
+        return toast.error(data.message);
       }
 
       sessionStorage.setItem(
         "data-sucess",
         JSON.stringify({
           title: "Código de verificação enviado",
-          description: "Confira o seu email e a caixa de spam para redefinir a sua senha.",
+          description:
+            "Confira o seu email e a caixa de spam para redefinir a sua senha.",
           button: {
             primary: {
               router: "/login",
-              name: "Voltar para a tela de login"
+              name: "Voltar para a tela de login",
             },
           },
         })
@@ -41,14 +53,14 @@ function RecuperarSenha() {
 
       router.push("/sucesso");
     } catch {
-      toast.error('Erro desconhecido');
+      toast.error("Erro desconhecido");
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
@@ -57,19 +69,15 @@ function RecuperarSenha() {
       subtitle="Enviaremos um código de verificação para o email abaixo"
       overflowAuto={true}
     >
-      <div className='w-full flex p-3 flex-col justify-start items-center gap-9 mt-3'>
-        <form className='w-full' onSubmit={handleSubmit(onSubmit)}>
-          <Input
+      <div className="w-full flex flex-col justify-start items-center gap-9 mt-3">
+        <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
+          <CustomInput
             label="E-mail"
-            labelClassName="pb-1.5 text-sm font-inter font-normal leading-5 tracking-tight"
-            register={{ ...register("email") }}
-            autoComplete="email"
-            className="text-slate-gray w-full"
-            placeholder='Digite seu e-mail'
+            register={register("email")}
+            placeholder="Digite seu e-mail"
+            errorMessage={errors.email?.message}
           />
-          <ButtonV2 variant='default'>
-            Avançar
-          </ButtonV2>
+          <ButtonV2 variant="default">Avançar</ButtonV2>
         </form>
       </div>
     </ModelPage>
