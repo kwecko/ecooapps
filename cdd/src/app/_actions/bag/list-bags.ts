520056b2d0fa2f9c3c6fdea5a49e4fe335dc1e03
zelindro <zelindroarthur@gmail.com>
2024-10-29 23:43:22 -0300

    feat: new status url model

--- cdd/src/app/_actions/bag/list-bags.ts
+++ cdd/src/app/_actions/bag/list-bags.ts
@@ -10,7 +10,7 @@ interface ListBagsRequest {
 }
 
 export async function listBags({ page, cycle_id, statuses, name }: ListBagsRequest) {
-  const statusParams = statuses.map((singleStatus) => `statuses[]=${singleStatus}`).join('&');
+  const statusParams = `statuses=` + statuses.map((singleStatus) => `${singleStatus}`).join(',');
   const url = `/bags?page=${page}&cycle_id=${cycle_id}&${statusParams}${name ? `&name=${name}` : ''}`;
 
   const response = ApiService.GET({
