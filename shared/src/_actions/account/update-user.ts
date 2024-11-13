e6161d5d3711876bbe6267a597e8ce1141922c85
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-11-13 09:23:30 -0300

    fix: use the correct interface in order to update users

--- shared/src/_actions/account/update-user.ts
+++ shared/src/_actions/account/update-user.ts
@@ -1,9 +1,9 @@
 "use server";
 
+import { IUserUpdate } from "@shared/interfaces/user";
 import ApiService from "@shared/service/index";
-import { IUser } from "@shared/interfaces/user";
 
-export async function updateUser(data: IUser) {
+export async function updateUser(data: IUserUpdate) {
   const response = ApiService.PATCH({
     url: "/users",
     data,
