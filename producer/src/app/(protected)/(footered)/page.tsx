6ba32e3b25f8b37db5e6db5711416eefc3d1304e
lucaslimasz <id.lucas@outlook.com.br>
2024-11-25 22:43:58 -0300

    test: header

--- producer/src/app/(protected)/(footered)/page.tsx
+++ producer/src/app/(protected)/(footered)/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 // import CycloInformation from "./home/components/CycloInformation";
-// import { Header } from "./home/components/Header";
+import { Header } from "./home/components/Header";
 import { PendingDeliveries } from "./home/components/PendingDeliveries";
 // import { ProductMenu } from "./home/components/ProductMenu";
 import SelectCycle from "@shared/components/SelectCycle";
@@ -12,13 +12,12 @@ export default function Home() {
   return (
     <div className="px-4 pb-10 pt-10 h-[var(--min-page-height)]">
       <div className='flex flex-col gap-5'>
-        {/* <Header /> */}
+        <Header />
         <SelectCycle />
         {/* <CycloInformation /> */}
         {/* <ProductMenu /> */}
         <PendingDeliveries />
       </div>
-      <h1>Bem vindo</h1>
     </div>
   );
 }
