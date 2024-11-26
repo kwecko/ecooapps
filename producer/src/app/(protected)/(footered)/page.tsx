7a9c21010e107065f43d4744f50f8155447e8d3d
lucaslimasz <id.lucas@outlook.com.br>
2024-11-25 22:26:44 -0300

    test: select cycle

--- producer/src/app/(protected)/(footered)/page.tsx
+++ producer/src/app/(protected)/(footered)/page.tsx
@@ -5,17 +5,17 @@ import React from 'react';
 // import { Header } from "./home/components/Header";
 import { PendingDeliveries } from "./home/components/PendingDeliveries";
 // import { ProductMenu } from "./home/components/ProductMenu";
-// import SelectCycle from "@shared/components/SelectCycle";
+import SelectCycle from "@shared/components/SelectCycle";
 
 export default function Home() {
 
   return (
     <div className="px-4 pb-10 pt-10 h-[var(--min-page-height)]">
       <div className='flex flex-col gap-5'>
-        {/* <Header />
+        {/* <Header /> */}
         <SelectCycle />
-        <CycloInformation />
-        <ProductMenu /> */}
+        {/* <CycloInformation /> */}
+        {/* <ProductMenu /> */}
         <PendingDeliveries />
       </div>
       <h1>Bem vindo</h1>
