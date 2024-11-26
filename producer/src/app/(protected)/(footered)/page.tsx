7101bbb43b757e3366db507a001f1953feedb80c
lucaslimasz <id.lucas@outlook.com.br>
2024-11-25 22:18:49 -0300

    test: pending deliveries

--- producer/src/app/(protected)/(footered)/page.tsx
+++ producer/src/app/(protected)/(footered)/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 // import CycloInformation from "./home/components/CycloInformation";
 // import { Header } from "./home/components/Header";
-// import { PendingDeliveries } from "./home/components/PendingDeliveries";
+import { PendingDeliveries } from "./home/components/PendingDeliveries";
 // import { ProductMenu } from "./home/components/ProductMenu";
 // import SelectCycle from "@shared/components/SelectCycle";
 
@@ -11,13 +11,13 @@ export default function Home() {
 
   return (
     <div className="px-4 pb-10 pt-10 h-[var(--min-page-height)]">
-      {/* <div className='flex flex-col gap-5'>
-        <Header />
+      <div className='flex flex-col gap-5'>
+        {/* <Header />
         <SelectCycle />
         <CycloInformation />
-        <ProductMenu />
+        <ProductMenu /> */}
         <PendingDeliveries />
-      </div> */}
+      </div>
       <h1>Bem vindo</h1>
     </div>
   );
