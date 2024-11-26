95d5ad24b798dab07bd20dfa89581e767aa6353f
lucaslimasz <id.lucas@outlook.com.br>
2024-11-25 22:02:42 -0300

    test: page

--- producer/src/app/(protected)/(footered)/page.tsx
+++ producer/src/app/(protected)/(footered)/page.tsx
@@ -1,23 +1,23 @@
 'use client'
 
 import React from 'react';
-import CycloInformation from "./home/components/CycloInformation";
-import { Header } from "./home/components/Header";
-import { PendingDeliveries } from "./home/components/PendingDeliveries";
-import { ProductMenu } from "./home/components/ProductMenu";
-import SelectCycle from "@shared/components/SelectCycle";
+// import CycloInformation from "./home/components/CycloInformation";
+// import { Header } from "./home/components/Header";
+// import { PendingDeliveries } from "./home/components/PendingDeliveries";
+// import { ProductMenu } from "./home/components/ProductMenu";
+// import SelectCycle from "@shared/components/SelectCycle";
 
 export default function Home() {
 
   return (
     <div className="px-4 pb-10 pt-10 h-[var(--min-page-height)]">
-      <div className='flex flex-col gap-5'>
+      {/* <div className='flex flex-col gap-5'>
         <Header />
         <SelectCycle />
         <CycloInformation />
         <ProductMenu />
         <PendingDeliveries />
-      </div>
+      </div> */}
     </div>
   );
 }
