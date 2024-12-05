b81b2cb5d6b8ebdea9c7a1058a0acf67b98d2d6a
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-12-05 16:50:01 -0300

    feat: create zod schema to validate password changing form

---
+++ shared/src/schemas/change-password/index.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const changePasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: "Senha deve ter no mínimo 8 caracteres" }),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (data: ChangePasswordSchema) => data.password === data.confirmPassword,
+    {
+      message: "Senhas não conferem",
+      path: ["confirmPassword"],
+    }
+  );
+
+export type ChangePasswordSchema = z.infer<typeof changePasswordSchema>;
