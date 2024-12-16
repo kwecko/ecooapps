4d2f8b4897ffec57a6dc00c8f0c11769b2c85607
zelindro <zelindroarthur@furg.br>
2024-12-16 17:29:21 -0300

    feature: report page layout (missing integration)

---
+++ admin/src/app/(protected)/(sidebar)/relatorios/components/DateInput.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { FiCalendar } from 'react-icons/fi';
+
+interface DateInputProps {
+  disabled?: boolean
+  label?: string;
+  value?: string;
+}
+
+export default function DateInput({ label, value, disabled = false }: DateInputProps) {
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // onChange(maskDate(e.target.value));
+  };
+
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="block text-sm text-theme-primary font-inter mb-2">
+          {label}
+        </label>
+      )}
+      <div className="relative">
+        <input
+          type="text"
+          value={value}
+          onChange={handleDateChange}
+          maxLength={10}
+          placeholder="DD / MM / YYYY"
+          disabled={disabled}
+          className={`w-full h-12 pl-3 pr-4 border border-theme-primary rounded-md placeholder-[#9BA5B7] text-theme-primary focus:outline-none focus:ring-0 focus:border-slate-gray ${disabled && 'opacity-50 cursor-not-allowed'}`}
+        />
+        <FiCalendar className="absolute right-3 top-3 text-[#9BA5B7]" size={25} />
+      </div>
+    </div>
+  );
+}
--- admin/src/app/(protected)/(sidebar)/relatorios/page.tsx
+++ admin/src/app/(protected)/(sidebar)/relatorios/page.tsx
@@ -1,5 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
 import Title from "@admin/app/components/Title";
+import SelectInput from "./components/SelectInput";
+import DateInput from "./components/DateInput";
+import Button from "@shared/components/Button";
+import Copyright from "@admin/app/components/Copyright";
+
 
 export default function page() {
-  return <Title>Emitir relatórios</Title>;
+
+  const [reportType, setReportType] = useState<string | undefined>(undefined);
+  const reportTypeOptions = [
+    { value: "ECONOMY", label: "Fluxo de caixa" },
+    { value: "EXPENSES", label: "Despesas" },
+  ];
+
+  const [currentStatus, setCurrentStatus] = useState<string | undefined>(
+    undefined
+  );
+
+  return (
+    <div className="flex flex-col w-full gap-6">
+      <Title>Emitir relatórios</Title>
+      <div className="w-2/5 flex flex-col gap-4">
+        <SelectInput
+          label="Selecione o tipo de relatório"
+          options={reportTypeOptions}
+          onChange={(value) => {
+            setReportType(value);
+          }}
+          defaultOption={reportTypeOptions[0]}
+        />
+
+        <SelectInput
+          label="Configurações adicionais"
+          options={reportTypeOptions}
+          onChange={() => {}}
+          disabled={true}
+          defaultOption={{ value: "No options", label: "Sem opções disponíveis" }}
+        />
+
+        <div className="flex gap-4">
+          <DateInput
+            label="Data inicial"
+          />
+          <DateInput
+            label="Data inicial"
+          />
+        </div>
+        
+        <Button 
+          className="w-full h-12 mt-4 rounded-md font-inter font-semibold text-white bg-rain-forest"
+          onChange={() => {}}
+        >
+          Emitir relatório
+        </Button>
+          
+        <div className="mt-80 mr-25">
+          <Copyright type="primary" />
+        </div>
+
+      </div>
+    </div>
+  );
 }
