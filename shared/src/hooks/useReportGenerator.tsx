3b254ca46844a78b4787e767103a5ff14aa2a100
Murilo Vargas <muriraubach@gmail.com>
2024-12-19 00:05:22 -0300

    Refactor useReportGenerator to support generating admin reports

--- shared/src/hooks/useReportGenerator.tsx
+++ shared/src/hooks/useReportGenerator.tsx
@@ -1,11 +1,21 @@
 import { toast } from "sonner";
 
 import { printDeliveriesReport } from "@shared/_actions/bags/GET/print-deliveries-report";
+import { fetchSalesReport } from "@shared/_actions/bags/GET/fetch-sales-report";
+
 import { useHandleError } from "@shared/hooks/useHandleError";
-import { ReportActions } from "@shared/types/report";
+import { AdminReportActions, ReportActions } from "@shared/types/report";
 
 export function useReportGenerator() {
   const { handleError } = useHandleError();
+
+  const adminReportActions: Record<
+    AdminReportActions,
+    (date_from: string, date_to: string) => Promise<any>
+  > = {
+    sales: (date_from: string, date_to: string) =>
+      fetchSalesReport({ date_from, date_to }),
+  };
 
   const reportActions: Record<ReportActions, (id: string) => Promise<any>> = {
     "list-bags": (id: string) => printDeliveriesReport({ cycle_id: id }),
@@ -21,6 +31,41 @@ export function useReportGenerator() {
     "offer-history": () => {
       return Promise.resolve();
     },
+  };
+
+  const generateAdminReport = async (
+    type: AdminReportActions,
+    date_from: string,
+    date_to: string
+  ) => {
+    try {
+      const action = adminReportActions[type];
+
+      if (!action) {
+        toast.error("Tipo de relatório desconhecido.");
+        return;
+      }
+
+      const response = await action(date_from, date_to);
+
+      if (response?.message) {
+        const messageError = response.message as string;
+        handleError(messageError);
+      } else {
+        const buffer = Buffer.from(response.data);
+
+        const blob = new Blob([buffer], {
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = `relatorio-${type}.xlsx`;
+        link.click();
+      }
+    } catch (error) {
+      toast.error("Erro desconhecido.");
+    }
   };
 
   const generateReport = async (type: ReportActions, id: string) => {
@@ -51,5 +96,5 @@ export function useReportGenerator() {
     }
   };
 
-  return { generateReport };
+  return { generateAdminReport, generateReport };
 }
