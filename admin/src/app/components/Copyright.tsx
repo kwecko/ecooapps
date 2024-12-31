1c61ef097e89b875a56c0d496b0ad8dfb1ed52f1
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-12-31 01:46:57 -0300

    refactor: add className prop to Copyright component and fix typo mistake 'LogoDard' to 'LogoDark'

--- admin/src/app/components/Copyright.tsx
+++ admin/src/app/components/Copyright.tsx
@@ -1,17 +1,22 @@
-import LogoDard from "@shared/assets/svg/logo-dark"
+import LogoDark from "@shared/assets/svg/logo-dark";
+import { twMerge } from "tailwind-merge";
 
 type CopyrightTypes = "primary" | "secondary";
 
 interface CopyrightProps {
   type: CopyrightTypes;
+  className?: string;
 }
 
-export default function Copyright({ type }: CopyrightProps) {
+export default function Copyright({ type, className }: CopyrightProps) {
   return (
-    <div className="w-full flex items-center gap-9 justify-center">
-      {type === "primary" && (
-        <LogoDard />
+    <div
+      className={twMerge(
+        "w-full flex items-center gap-9 justify-center",
+        className
       )}
+    >
+      {type === "primary" && <LogoDark />}
       <span
         className={`${
           type === "primary" ? "text-left" : "text-center"
