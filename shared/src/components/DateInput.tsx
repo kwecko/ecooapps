7971733e5130ebe9ad89f7ff8609401eb0a7e30d
Murilo Vargas <muriraubach@gmail.com>
2025-01-12 20:24:56 -0300

    Update package.json and package-lock.json

--- shared/src/components/DateInput.tsx
+++ shared/src/components/DateInput.tsx
@@ -1,37 +1,43 @@
 "use client";
 
-import { FiCalendar } from 'react-icons/fi';
+import { FiCalendar } from "react-icons/fi";
 
 interface DateInputProps {
-  disabled?: boolean
   label?: string;
-  value?: string;
+  value?: Date;
+  disabled?: boolean;
+  onChange: (date: Date) => void;
 }
 
-export default function DateInput({ label, value, disabled = false }: DateInputProps) {
-
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // onChange(maskDate(e.target.value));
-  };
-
+export default function DateInput({
+  label,
+  value,
+  disabled = false,
+  onChange,
+}: DateInputProps) {
   return (
     <div className="w-full">
       {label && (
-        <label className="block text-sm text-theme-primary mb-2">
+        <label className="block text-sm font-normal text-gray-600 mb-1">
           {label}
         </label>
       )}
       <div className="relative">
         <input
-          type="text"
-          value={value}
-          onChange={handleDateChange}
-          maxLength={10}
-          placeholder="DD/MM/YYYY"
+          type="date"
+          value={value ? value.toISOString().split("T")[0] : ""}
+          onChange={(e) => {
+            const date = e.target.value ? new Date(e.target.value) : undefined;
+            if (!date) return;
+            onChange(date);
+          }}
           disabled={disabled}
-          className={`w-full h-12 pl-3 pr-4 border border-gray-300 rounded-md placeholder-[#9BA5B7] text-theme-primary focus:outline-none focus:ring-0 focus:border-slate-gray ${disabled && 'opacity-50 cursor-not-allowed'}`}
+          className={`w-full h-12 px-3 border border-slate-gray rounded-md font-inter text-theme-primary
+            placeholder:text-gray-400 
+            focus:outline-none focus:ring-1 focus:ring-gray-400
+            disabled:bg-gray-100 disabled:cursor-not-allowed
+            ${disabled && "opacity-60"}`}
         />
-        <FiCalendar className="absolute right-3 top-3.5 text-[#9BA5B7]" />
       </div>
     </div>
   );
