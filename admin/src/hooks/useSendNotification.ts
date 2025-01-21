b3617d7c5792f911d57ca41fd7f84dc3b3572a55
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-01-21 01:34:57 -0300

    feat: create use send notification hook

---
+++ admin/src/hooks/useSendNotification.ts
@@ -0,0 +1,39 @@
+import {
+  SendNotificationRequest,
+  sendNotification as sendNotificationAction,
+} from "@admin/_actions/notifications/POST/send-notification";
+import { useHandleError } from "@shared/hooks/useHandleError";
+import { useState } from "react";
+interface UseSendNotificationProps extends SendNotificationRequest {}
+
+export default function useSendNotification() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { handleError } = useHandleError();
+
+  const sendNotification = async (
+    data: UseSendNotificationProps
+  ): Promise<boolean> => {
+    setIsLoading(true);
+    let success: boolean = false;
+    await sendNotificationAction(data)
+      .then((response) => {
+        if (response.message) {
+          const messageError = response.message as string;
+          handleError(messageError);
+          success = false;
+        } else {
+          success = true;
+        }
+      })
+      .catch(() => {
+        handleError("Erro ao enviar a notificação.");
+        success = false;
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    return success;
+  };
+
+  return { sendNotification, isLoading };
+}
