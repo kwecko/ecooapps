4edba75701c1ea158c8d3fb4360bdac300e208a0
giovanni <br.giovanni@gmail.com>
2025-04-19 01:33:18 -0300

    fix: remove imageLoader and add photo and images in farm-dto

--- consumer/src/app/(purchase)/produtor/components/FarmPhotos.tsx
+++ consumer/src/app/(purchase)/produtor/components/FarmPhotos.tsx
@@ -1,24 +1,24 @@
-import Image, { ImageLoader } from "next/image";
+import Image from "next/image";
 
-export default function FarmPhotos(images: string[]) {
-  return (
-    <>
-        {images && images.length !== 0
-          ? images.map((image) => {
-              return (
-                <div className="w-full">
-                    <Image
-                    src={image}
-                    width={100}
-                    height={100}
-                    alt={`${image}.jpg`}
-                    />
-                </div>
-              );
-            })
-          :
-          <div className="text-center">Não possui imagens</div>
-          }
-    </>
-  );
+export default function FarmPhotos(images: string[] | null) {
+	return (
+		<>
+			{images && images.length !== 0
+				? images.map((image) => {
+					return (
+						<div className="w-full">
+							<Image
+								src={image}
+								width={100}
+								height={100}
+								alt={`${image}.jpg`}
+							/>
+						</div>
+					);
+				})
+				:
+				<div className="text-center">Não possui imagens</div>
+			}
+		</>
+	);
 }
