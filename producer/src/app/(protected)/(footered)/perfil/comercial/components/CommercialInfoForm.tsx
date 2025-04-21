52fc9130e100bff312076a2b2bfaee84f354041f
Murilo Vargas <muriraubach@gmail.com>
2025-04-21 17:11:43 -0300

    fix: ensure URL object is converted to string in image source

--- producer/src/app/(protected)/(footered)/perfil/comercial/components/CommercialInfoForm.tsx
+++ producer/src/app/(protected)/(footered)/perfil/comercial/components/CommercialInfoForm.tsx
@@ -126,7 +126,7 @@ function CommercialInfoForm({ photo, images, setPhoto, setImages, sendImage, rem
                   id={`photo-${index}`}
                   key={index}
                   priority
-                  src={typeof images[index] === "string" ? images[index] : URL.createObjectURL(images[index])}
+                  src={typeof images[index] === "string" ? images[index] : URL.createObjectURL(images[index]).toString()}
                   alt="User"
                   width={80}
                   height={80}
