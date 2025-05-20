69e5b7536d256d308a0bf21488097d3c2bf39795
giovanni <br.giovanni@gmail.com>
2025-05-20 00:12:20 -0300

    fix: adjusting mini app theme problem

--- consumer/src/app/(purchase)/layout.tsx
+++ consumer/src/app/(purchase)/layout.tsx
@@ -5,11 +5,10 @@ import Header from "../_components/header";
 import { TelegramProvider } from "@consumer/context/telegram";
 
 export default function Produtores({ children }: React.PropsWithChildren) {
-  console.log("ProdutoresLayout carregado!");
   return (
     <TelegramProvider>
       <CartProvider>
-        <div className="w-full h-screen overflow-hidden">
+        <div className="w-full h-screen overflow-hidden bg-white">
           <Header />
           {children}
         </div>
