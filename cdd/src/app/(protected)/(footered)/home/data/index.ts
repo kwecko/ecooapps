ebbbf02d7532b1eb5a5fdf2b3f0ff33dbf463d17
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-07-27 16:56:21 -0300

    feat: add information handling to `CardComponent` and update `contentLinksHomePage`

--- cdd/src/app/(protected)/(footered)/home/data/index.ts
+++ cdd/src/app/(protected)/(footered)/home/data/index.ts
@@ -1,27 +1,60 @@
-export const contentLinksHomePage = [
+import { InfoIconModalProps } from "@shared/components/InfoIconModal";
+
+export interface ContentLink {
+  title: string;
+  link: string;
+  disabled: boolean;
+  isSelectedCycle?: boolean;
+  hasNotification?: boolean;
+  information?: InfoIconModalProps;
+}
+
+export const contentLinksHomePage: ContentLink[] = [
   {
     title: "Receber ofertas",
     link: "/ofertas",
+    disabled: false,
     isSelectedCycle: true,
-    hasNotification: false
+    hasNotification: false,
+    information: {
+      title: "Receber Ofertas",
+      content:
+        "Verifique os itens recebidos dos produtores, aprovando ou rejeitando ofertas.",
+    },
   },
   {
     title: "Montar sacola",
     link: "/montar-sacola",
+    disabled: false,
     isSelectedCycle: true,
-    hasNotification: false
+    hasNotification: false,
+    information: {
+      title: "Montar Sacola",
+      content:
+        "Veja quais sacolas foram montadas e registre as sacolas que já foram montadas.",
+    },
   },
   {
     title: "Enviar sacola",
     link: "/enviar-sacola",
+    disabled: false,
     isSelectedCycle: true,
-    hasNotification: false
+    hasNotification: false,
+    information: {
+      title: "Enviar Sacola",
+      content:
+        "Veja as sacolas disponíveis para serem enviadas e registre as sacolas já enviadas.",
+    },
   },
   {
     title: "Gerar relatórios",
     link: "/relatorios",
+    disabled: false,
     isSelectedCycle: true,
     hasNotification: false,
-    disabled: false
+    information: {
+      title: "Gerar Relatórios",
+      content: "Baixe relatórios relativos às atividades do CDD.",
+    },
   },
-]
+];
