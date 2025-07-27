6c1cf9298bbbdbba92b56b828ac52cce0fbb87bb
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-07-27 16:59:04 -0300

    feat: extend `ReportButton` interface with information property in `reportButtonData`

--- cdd/src/app/(protected)/(footered)/relatorios/data/index.ts
+++ cdd/src/app/(protected)/(footered)/relatorios/data/index.ts
@@ -1,19 +1,33 @@
-import { ReportButtonData } from "@shared/types/report/index";
+import { ReportButton } from "@shared/types/report/index";
 
-export const reportButtonData: ReportButtonData = [
+interface ReportButtonProps extends ReportButton {}
+
+export const reportButtonData: ReportButtonProps[] = [
   {
     name: "Lista de Entregas",
-    onClick: 'list-bags',
-    disabled: false
+    onClick: "list-bags",
+    disabled: false,
+    information: {
+      title: "Lista de Entregas",
+      content: "Baixe o relatório de lista de entregas.",
+    },
   },
   {
     name: "Retirada de Sacolas",
     onClick: "list-bags-withdrawn",
-    disabled: false
+    disabled: false,
+    information: {
+      title: "Retirada de Sacolas",
+      content: "Baixe o relatório de retirada de sacolas.",
+    },
   },
   {
     name: "Recebimento de Ofertas",
     onClick: "fetch-inbound",
-    disabled: false
+    disabled: false,
+    information: {
+      title: "Recebimento de Ofertas",
+      content: "Baixe o relatório de recebimento de ofertas.",
+    },
   },
 ];
